ed7418cc805929d864cf5c5bb45800be5ed851b3
Jeremiah A. Bohling <jeremiah.bohling@gmail.com>
2022-11-02 19:06:08 -0500

    Initial commit

---
+++ spec/amygdala_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Amygdala do
+  it "has a version number" do
+    expect(Amygdala::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
